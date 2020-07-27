a3313fdcb5512986e5f16902772448e91014e816
Raphael Ferrao <raphael.ferrao@thomsonreuters.com>
2020-07-27 14:09:45 -0300

    Setup Inicial do Projeto

---
+++ src/components/Menu/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Logo from '../../assets/img/Logo.png';
+import Button from '../Button';
+import './Menu.css';
+
+
+const Menu = () => {
+    return (
+        <nav className="Menu" >
+            <a href="/" >
+                <img className="Logo" src={Logo} alt="FerraoFlix logo" />
+            </a>
+
+            <Button as="a" className="ButtonLink" href="/" >
+                Novo Vídeo
+            </Button>
+        </nav>
+    );
+};
+
+export default Menu;
